dcf39bf6327b5509563467c45baca8c18aaa84a5
Luke Alberto <lalberto@terpmail.umd.edu>
2017-11-08 00:11:28 -0500

    luke  add login validation
    -added js validation for account creation

--- js/login_page.js
+++ js/login_page.js
@@ -21,9 +21,32 @@ function validateLogin(theForm) {
 	}
 }
 
-/* 
-*
+/* JS validation for creating account. Only checks if username is "demo" or
+* passwords do not match
 */
 function validateCreateAccount(theForm){
 	//
+	var username  = theForm.username.value;
+	var email = theForm.email.value;
+	var password = theForm.password.value;
+	var confirm_password = theForm.confirm_password.value;
+
+	var error = "";
+
+	//need to change this eventually
+	if (username === "demo"){
+		error += "The username you have entered already exists \n";
+	}
+
+	if (password !== confirm_password) {
+		error += "The passwords you have entered do not match \n";
+	}
+
+	if (error === ""){
+		return true;
+	}else{
+		$("#creation-error").html("<div class='col-sm-8 danger-text'>" + error + "</div>");
+		$("#creation-error").removeAttr("hidden");
+		return false;
+	}
 }
