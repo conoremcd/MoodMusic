21a6e5c7dcda0ad7ebaa7fd105cb3ff85555bb01
conormcd7 <conormcd7@users.noreply.github.com>
2017-11-29 19:15:00 -0500

    Update colorRequestor.php

--- colorRequestor.php
+++ colorRequestor.php
@@ -1,38 +1,70 @@
-	<!DOCTYPE html>
-	
-	<html>
+<!DOCTYPE HTML>
+<html lang="en">
 	<head>
-		<title>Order Request Form</title>
-		<link rel="stylesheet" title="formStyle" />
+	  <title>Color Selection</title>
+	  <meta charset="utf-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1">
+	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	  <link rel="stylesheet" href="css/page_template.css">
+	  <link rel="stylesheet" href="css/color_select.css">
+	  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+	  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	  <script type="text/javascript" src="js/audioPlayer.js"></script>
+      <script src="https://code.jquery.com/jquery-2.2.0.js"></script>
 	</head>
-	
 	<body>
-		
-		
-		
-		<form action="m_player.php" method="post">        
-			
-			<section class="boxed">
-			
-			<h1><u>Color Requestor</u></h1>
-											
-				<p>
-					<h3>Color Preference</h3><br />
-						Red<input type="radio" name="colorPref" value="red" checked="checked" />&nbsp;
-						Yellow<input type="radio" name="colorPref" value="yellow" />&nbsp;
-						Blue<input type="radio" name="colorPref" value="blue" />&nbsp;
-						Purple<input type="radio" name="colorPref" value="purple" />&nbsp;
-						Orange<input type="radio" name="colorPref" value="orange" />&nbsp;
-						Green<input type="radio" name="colorPref" value="green" />&nbsp;
-				</p>
-			
-		
-		
-		</section>
-        
-        <p>
-				<input type="submit" value="Submit Request" />
-		</p>
-    
+		<header class="container-fluid header">
+			<div class="row visible-xs-block" id="mobile-header">
+				<div class="col-xs-1"></div>
+				<div class="col-xs-2">
+					<a href=""><img src="images/logo_placeholder.png" width="40" height="40" alt=""></a>
+				</div>
+				<div class="col-xs-9">
+					<h5 class="lt-h horizontal align">How Do You Feel?</h5>
+				</div>
+			</div>
+		</header>
+		<div class="container-fluid pnl">
+			<form action="m_player.php" method="post">        
+					<div class="row pnl bmprbx">
+						<div class="col-sm-3"></div>
+						<div class="col-xs-4 col-sm-2 horizontal-align bmprbx">
+							<button class="clr-slct-btn rd" name="colorPref" value="red"></button>
+						</div>
+						<div class="col-xs-4 col-sm-2 horizontal-align bmprbx">
+							<button class="clr-slct-btn orng" name="colorPref" value="orange"></button>
+						</div>
+						<div class="col-xs-4 col-sm-2 horizontal-align bmprbx">
+							<button class="clr-slct-btn yllw" name="colorPref" value="yellow"></button>
+						</div>
+						
+						<div class="col-sm-3"></div>
+					</div>
+					<div class="row pnl bmprbx">						
+						<div class="col-sm-3"></div>
+						<div class="col-xs-4 col-sm-2 horizontal-align bmprbx">
+							<button class="clr-slct-btn grn" name="colorPref" value="green"></button>
+						</div>
+						<div class="col-xs-4 col-sm-2 horizontal-align bmprbx">
+							<button class="clr-slct-btn blu" name="colorPref" value="blue"></button>
+						</div>
+						<div class="col-xs-4 col-sm-2 horizontal-align bmprbx">
+							<button class="clr-slct-btn prpl" name="colorPref" value="purple"></button>
+						</div>
+						<div class="col-sm-3"></div>
+					</div>
+					<div class="row pnl">
+						<div class="col-sm-9"></div>
+						<div class="col-xs-12 col-sm-2">
+							<button class="cnfrm-btn btn btn-md btn-block" type="submit">
+								Load Playlist
+							</button>
+						</div>
+						<div class="col-sm-1"></div>
+					</div>
+			</form>
+		</div>
 	</body>
-	</html>
+</html>
